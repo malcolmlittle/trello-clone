f23a1a715259740c0e09daee49a067475e419007
Malcolm Little <malcolmvlittle@gmail.com>
2022-06-22 18:32:46 -0700

    added ANI comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { AddNewItem } from "./AddNewItem";
 import { Column } from "./Column";
 import { AppContainer } from "./styles";
 
 export function App() {
   return <AppContainer>
     <Column text="Todo:" />
+    <AddNewItem toggleButtonText="+ Add another list" onAdd={() => console.log("Item created")} />
   </AppContainer>
 }
