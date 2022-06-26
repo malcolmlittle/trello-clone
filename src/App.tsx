c09908cda90ea4621ee13907570477e762b6667a
Malcolm Little <malcolmvlittle@gmail.com>
2022-06-26 16:59:41 -0700

    render column comps from useAppState lists obj

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { AddNewItem } from "./AddNewItem";
 import { Column } from "./Column";
 import { AppContainer } from "./styles";
+import { useAppState } from "./state/AppStateContext";
 
-export function App() {
-  return <AppContainer>
-    <Column text="Todo:" />
-    <AddNewItem toggleButtonText="+ Add another list" onAdd={(e) => console.log(e)} />
-  </AppContainer>
+export const App = () => {
+  const { lists } = useAppState()
+
+  return (
+    <AppContainer>
+      {lists.map(list => (
+        <Column text={list.text} key={list.id} id={list.id} />
+      ))}
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={(e) => console.log(e)} />
+    </AppContainer>
+  )
 }
